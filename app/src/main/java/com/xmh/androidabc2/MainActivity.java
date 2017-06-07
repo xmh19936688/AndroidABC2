a11f5be952ad976cdca12b2437cc8c11ab8f47a6
voidXMH <xmh19936688@163.com>
2017-06-07 17:07:14 +0800

    add local-broad-cast

--- app/src/main/java/com/xmh/androidabc2/MainActivity.java
+++ app/src/main/java/com/xmh/androidabc2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.xmh.androidabc2;
 
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -20,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         sendOrderedBroadcast(new Intent().setAction("action"),null);
         //send ordered broadcast with permission
         sendOrderedBroadcast(new Intent().setAction("action"),"permission");
+    }
+
+    private void localBroad(){
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        //register
+        localBroadcastManager.registerReceiver(null,null);
+        //unregister
+        localBroadcastManager.unregisterReceiver(null);
+
+        //send normal broadcast
+        localBroadcastManager.sendBroadcast(new Intent().setAction("action"));
+        //send synchronized broadcast
+        localBroadcastManager.sendBroadcastSync(new Intent().setAction("action"));
     }
 
     @Override
