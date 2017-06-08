f28cd559b165f5bbffcd05647844e7762a76389f
voidXMH <xmh19936688@163.com>
2017-06-08 14:37:23 +0800

    add shared-preference

--- app/src/main/java/com/xmh/androidabc2/MainActivity.java
+++ app/src/main/java/com/xmh/androidabc2/MainActivity.java
@@ -1,9 +1,12 @@
 package com.xmh.androidabc2;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +38,22 @@ public class MainActivity extends AppCompatActivity {
         localBroadcastManager.sendBroadcast(new Intent().setAction("action"));
         //send synchronized broadcast
         localBroadcastManager.sendBroadcastSync(new Intent().setAction("action"));
+    }
+
+    private void sharedPreferences(){
+        Context app=getApplicationContext();
+        app.getSharedPreferences("name",MODE_PRIVATE);
+        //:/data/data/{package}/shared_prefs/
+
+        Activity activity=this;
+        activity.getPreferences(MODE_PRIVATE);
+        //will call
+        getSharedPreferences(getLocalClassName(), MODE_PRIVATE);
+
+        PreferenceManager.getDefaultSharedPreferences(app);
+        PreferenceManager.getDefaultSharedPreferences(activity);
+        //will call
+        app.getSharedPreferences(app.getPackageName() + "_preferences", MODE_PRIVATE);
     }
 
     @Override
